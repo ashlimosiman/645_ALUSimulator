ded65824540d7e2a9bd9d89ba988e59dac64ccf5
Ashli Mosiman <amosiman93@gmail.com>
2017-05-01 16:52:58 -0500

    Made progress and then ran into a weird error

--- ALUSimulator.c
+++ ALUSimulator.c
@@ -2,82 +2,97 @@
 
 void ALUSimulator(RegisterFile theRegisterFile, uint32_t OpCode, uint32_t Rs, uint32_t Rt, uint32_t Rd, uint32_t ShiftAmt, uint32_t FunctionCode, uint32_t ImmediateValue, uint32_t* Status )
 {
-     if(!opCode)
+     switch(opCode)
      {
-          switch(FunctionCode)
-          {
-               case 0://SLL and NOOP
-                    Rd = Rt << ShiftAmt;
+          case 0:
+               switch(FunctionCode)
+               {
+                    case 0://SLL and NOOP
+                         Rd = Rt << ShiftAmt;
+                         break;
+                    case 2://SRL
+                         Rd = Rt >> ShiftAmt;
+                         break;
+                    case 3://SRA
+                         Rd = signed(Rt) >> ShiftAmt;
+                         break;
+                    case 4://SLLV
+                         Rd = Rt << Rs;
+                         break;
+                    case 6://SRLV
+                         Rd = Rt >> Rs;
+                         break;
+                    case 32://ADD
+                         Rd = Rs + Rt;//overflow is exception
+                         /*
+                         if(Rd<Rs || Rd<Rt)
+                         {
+                              uint32_t overflow=12;
+                              Status=overflow;
+                         }*/
+                         break;
+                    case 33://ADDU
+                         Rd = Rs + Rt;
+                         break;
+                    case 34://SUB
+                         Rd = Rs - Rt;
+                         break;
+                    case 35://SUBU
+                         Rd = Rs - Rt;//should be different from SUB?
+                         break;
+                    case 36://AND
+                         Rd = Rs & Rt;
+                         break;
+                    case 37://OR
+                         Rd = Rs | Rt;
+                         break;
+                    case 38://XOR
+                         Rd = Rs ^ Rt;
+                         break;
+                    case 42://SLT
+                         if(Rs < Rt)
+                         {
+                              Rd = 1;
+                         }
+                         else
+                         {
+                              Rd = 0;
+                         }
+                         break;
+                    case 43://SLTU
+                         if(Rd < Rt)//should be different from SLT?
+                         {
+                              Rd = 1;
+                         }
+                         else
+                         {
+                              Rd = 0;
+                         }
+                         break;
+                    }//end of "functionCode" switch
                     break;
-               case 2://SRL
-                    Rd = Rt >> ShiftAmt;
-                    break;
-               case 3://SRA
-                    Rd = signed(Rt) >> ShiftAmt;
-                    break;
-               case 4://SLLV
-                    Rd = Rt << Rs;
-                    break;
-               case 6://SRLV
-                    Rd = Rt >> Rs;
-               case 16://MFHI
 
-               case 18://MFLO
+          case 8://ADDI
+               break;
 
-               case 24://MULT
+          case 9://ADDIU
+               break;
 
-               case 25://MULTU
+          case 10://SLTI
+               break;
 
-               case 26://DIV
+          case 11://SLTIU
+               break;
+     }//end of "opCode" switch
+//Provided by Minden
+     printf( ">>ALU: Opcode: %02X; Rs: %02X; Rt: %02X; Rd: %02X;\n",
+				OpCode,
+				Rs,
+				Rt,
+				Rd );
 
-               case 27://DIVU
-
-               case 32://ADD
-                    Rd = Rs + Rt;
-                    break;
-               case 33://ADDU
-                    Rd = Rs + Rt;//should be different from ADD?
-                    break;
-               case 34://SUB
-                    Rd = Rs - Rt;
-                    break;
-               case 35://SUBU
-                    Rd = Rs - Rt;//should be different from SUB?
-                    break;
-               case 36://AND
-                    Rd = Rs & Rt;
-                    break;
-               case 37://OR
-                    Rd = Rs | Rt;
-                    break;
-               case 38://XOR
-                    Rd = Rs ^ Rt;
-                    break;
-               case 42://SLT
-                    if(Rs < Rt)
-                    {
-                         Rd = 1;
-                    }
-                    else
-                    {
-                         Rd = 0;
-                    }
-               case 43://SLTU
-                    if(Rd < Rt)//should be different from SLT?
-                    {
-                         Rd = 1;
-                    }
-                    else
-                    {
-                         Rd = 0;
-                    }
-          }//end of switch
-     }//end of "opCode" if
-     else
-     {
-          //ADDI
-          //ADDIU
-          //SLTI
-          //SLTIU
-     }
+	printf( ">>>>ALU: ShiftAmt: %02X; FunctionCode: %02X; ImmediateValue: %04X;\n",
+				ShiftAmt,
+				FunctionCode,
+				ImmediateValue );
 }//end of method
--- ALUSimulator.h
+++ ALUSimulator.h
@@ -1,28 +1,20 @@
-#ifndef ALU_SIMULATOR_H
-#define ALU_SIMULATOR_H
+//*****************************************************************************
+//--ALUSimulator.h
+//
+//		Author: 		Gary J. Minden
+//		Organization:	KU/EECS/EECS 645
+//		Date:			2017-04-22 (B70422)
+//		Version:		1.0
+//		Description:	This is the prototype for a simple ALU simulator
+//		Notes:
+//
+//*****************************************************************************
+//
 
-#include <stdio.h>
-#include <time.h>
-#include <strings.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-#include <stddef.h>
-#include <stdbool.h>
-#include <stdint.h>
-#include <stdarg.h>
-
-#include "RegisterFile_01.h"
-
-void ALUSimulator( RegisterFile theRegisterFile,
-                    uint32_t OpCode,
-                    uint32_t Rs, uint32_t Rt, uint32_t Rd,
-                    uint32_t ShiftAmt,
-                    uint32_t FunctionCode,
-                    uint32_t ImmediateValue,
-                    uint32_t* Status );
-
-
-
-
-#endif
+extern void ALUSimulator( RegisterFile theRegisterFile,
+				uint32_t OpCode,
+				uint32_t Rs, uint32_t Rt, uint32_t Rd,
+				uint32_t ShiftAmt,
+				uint32_t FunctionCode,
+				uint32_t ImmediateValue,
+				uint32_t* Status );
